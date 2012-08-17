ad0e8250838aabe1e6dde92e8e8802d6f42bbf99
Syed Faraz Ahmed <torpedo1>
2012-08-17 15:23:17 +0500

    New Files Addes

--- Sage Engine/Sage Engine/Tile Engine/TileLayer.cs
+++ Sage Engine/Sage Engine/Tile Engine/TileLayer.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using Microsoft.Xna.Framework.Graphics;
+using System.Collections;
 
 namespace Sage_Engine
 {
@@ -12,5 +13,34 @@ namespace Sage_Engine
         List<Texture2D> textureList = new List<Texture2D>();
 
         float alpha = 1f;
+
+       //Change These According to the tile Height and width Must Be Same for all classes.
+       private static  int TileWidth = 32;
+       private static  int TileHeight = 32;
+
+       public int LayerWidthinTiles
+       {
+           get
+           {
+               return layer.GetLength(1);
+           }
+       }
+
+       public int LayerHeightinTiles
+       {
+           get
+           {
+               return layer.GetLength(0);
+           }
+       }
+
+       public int LayerWidthInPixels
+       {
+           get
+           {
+
+           }
+       }
+
     }
 }
